544f54e3a6a419a834445d42c6cb34662691b094
root <root@DESKTOP-4BB2MNA.localdomain>
2022-09-27 12:19:37 +0200

    first commit

---
+++ tp.py
@@ -0,0 +1,11 @@
+#print (A)
+#print ("imc=",imc) comment calculer une masse corporelle
+imc = ("poids")
+poids=float(input("Poids en kg:"))
+taille=float(input("Taille en m:"))
+imc=poids/taille**2
+print ("imc=",imc)
+
+
+
+
